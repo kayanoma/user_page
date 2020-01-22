581c397c92f58a1a7b0d9b1bc33c6595dbd182eb
Ryota Kayanoma <z.ryota.kayanoma@gree.net>
2020-01-22 14:20:31 +0900

    websocket追加中

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-// var client = function(){
+function client(){
+  // var connection=new WebSocket('wss://6191df17.ngrok.io');
+  var connection=new WebSocket('ws://6191df17.ngrok.io');
+  connection.send('Hello');
+  connection.onmessage=function(e){
+    document.getElementById('return').innerText=e.data;
+  };
+  connection.close();
+};
 
-// };
 
-// const requestDeviceMotionPermission = () => {
-//   if (
-//     DeviceMotionEvent &&
-//     typeof DeviceMotionEvent.requestPermission === 'function'
-//   ) {
-//     // iOS 13+ の Safari
-//     // 許可を取得
-//     DeviceMotionEvent.requestPermission()
-//     .then(permissionState => {
-//       if (permissionState === 'granted') {
-//         // 許可を得られた場合、devicemotionをイベントリスナーに追加
-//         window.addEventListener('devicemotion', e => {
-//           // devicemotionのイベント処理
-//           e.acceleration.x
-//         })
-//       } else {
-//         // 許可を得られなかった場合の処理
-//         window.location.href='https://www.google.com/search?q=%E8%A8%B1%E5%8F%AF&rlz=1C1GCEU_jaJP858JP859&oq=%E8%A8%B1%E5%8F%AF&aqs=chrome..69i57j0l6j69i61.1720j1j7&sourceid=chrome&ie=UTF-8';
-//       }
-//     })
-//     .catch(console.error) // https通信でない場合などで許可を取得できなかった場合
-//   } else {
-//     window.location.href='https://www.google.com/search?q=%E8%A8%B1%E5%8F%AF&rlz=1C1GCEU_jaJP858JP859&oq=%E8%A8%B1%E5%8F%AF&aqs=chrome..69i57j0l6j69i61.1720j1j7&sourceid=chrome&ie=UTF-8';
-//     // 上記以外のブラウザ
-//   }
-// }
 
 function get_permission(){
   if (typeof DeviceMotionEvent.requestPermission === 'function') {
@@ -55,12 +36,5 @@ function get_permission(){
   }
 };
 
-// ボタンクリックでrequestDeviceMotionPermission実行
-// const startButton = document.getElementById("start-button")
-// startButton.addEventListener('click', requestDeviceMotionPermission, false)
-const startButton = document.getElementById("test")
-startButton.addEventListener('click', window.location.href='https:/www.google.com', false)
-function google(){
-  window.location.href='https:/www.google.com';
-};
-// window.location.href='https:/www.google.com'
+
+
