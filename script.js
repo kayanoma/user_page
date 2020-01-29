12d65ec553bcf51e7cce7c0a857ee6a4e082ae2e
Ryota Kayanoma <z.ryota.kayanoma@gree.net>
2020-01-29 17:30:07 +0900

    client side

--- script.js
+++ script.js
@@ -1,55 +1,18 @@
-// const express = require('express');
-// const app = express();
-// const socket=require('socket.io')
-// const expressServer = app.listen(3000); //clientのポート番号（localhost:3000)
-// const io = socketio(expressServer); 
 
-// function client(){
-//   var connection=new WebSocket('https://6191df17.ngrok.io');
+var URL
+function client(){
+  var connection=new WebSocket('https://6191df17.ngrok.io');
+  connection.addEventListener("open", e =>{
+    document.getElementById('start').innerText='start';
+    document.getElementById('state').innerText=connection.readyState;
+    connection.send('Hello');
+  });
+  connection.addEventListener("message", e=>{
+    document.getElementById('return').innerText=e.data;
+    connection.close;
+  });
+};
 
-  
-//   connection.addEventListener("open", e =>{
-//     document.getElementById('start').innerText='start';
-//     document.getElementById('state').innerText=connection.readyState;
-//     connection.send('Hello');
-//   });
-//   connection.addEventListener("message", e=>{
-//     document.getElementById('return').innerText=e.data;
-//     connection.close;
-//   });
-
-// };
-
-
-
-
-
-// function client(){
-//   // var connection=new WebSocket('wss://6191df17.ngrok.io');
-//   var connection=new WebSocket('https://6191df17.ngrok.io');
-//   // var connection=new WebSocket('wss://kayanoma.github.io/websocket_server/');
-//   // var connection=new WebSocket('http://127.0.0.1:3000');
-  
-//   connection.addEventListener("open", e =>{
-//     document.getElementById('start').innerText='start';
-//     document.getElementById('state').innerText=connection.readyState;
-//     connection.send('Hello');
-//   });
-//   connection.addEventListener("message", e=>{
-//     document.getElementById('return').innerText=e.data;
-//     connection.close;
-//   });
-//   // connection.onmessage=function(e){
-//   //   document.getElementById('return').innerText=e.data;
-//   // };
-//   // connection.close();
-//   // document.getElementById('state').innerText=connection.readyState;
-//   // connection.send('Hello');
-//   // connection.onmessage=function(e){
-//   //   document.getElementById('return').innerText=e.data;
-//   // };
-//   // connection.close();
-// };
 
 
 function get_permission(){
@@ -79,10 +42,104 @@ function get_permission(){
     document.getElementById('value_x').innerText='error not ios 13+';
     document.getElementById('value_y').innerText='';
     document.getElementById('value_z').innerText='';
-    // document.getElementById('value_y').innerText='error not ios 13+';
-    // document.getElementById('value_z').innerText='error not ios 13+';
   }
 };
 
 
+function getURL(){
+  URL=document.getElementById('URL').value;
+  // websocket connect
+  console.log(URL);
+  // window.open(URL);
+  document.getElementById('check').innerText=URL;
+}
 
+function get_permission2(){
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    // iOS 13+
+    
+    const io= require('./../set_up.js');
+    window.open(URL)
+      io.on('connection', (socket)=>{
+        console.log('made socket connection', socket.id);
+    
+        socket.on('echo', (arg) => {
+            console.log(arg);
+        })
+        socket.on('disconnect', () => {
+            console.log('disconnect');
+        });
+
+        DeviceMotionEvent.requestPermission()
+        .then(response => {
+      
+          const start=Date.now();
+          var now=start;
+          if (response == 'granted') {
+            
+            window.addEventListener('devicemotion', (e) => {
+              // do something with e
+              document.getElementById('time').innerText=Date.now()-start;
+              document.getElementById('delta_t').innerText=Date.now()-now;
+              now=Date.now();
+              document.getElementById('value_x').innerText=e.acceleration.x;
+              document.getElementById('value_y').innerText=e.acceleration.y;
+              document.getElementById('value_z').innerText=e.acceleration.z;
+              io.emit('accel', e.acceleration);
+            })
+          }
+        })
+        .catch(console.error)
+        // socket.on('client', (obj)=>{
+        //     obj.x;
+        //     obj.y;
+        //     obj.z;
+        // });
+    });
+  } else {
+    // non iOS 13+
+    document.getElementById('value_x').innerText='error not ios 13+';
+    document.getElementById('value_y').innerText='';
+    document.getElementById('value_z').innerText='';
+  }
+};
+
+function get_permission3(){
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    // iOS 13+
+
+
+      DeviceMotionEvent.requestPermission()
+      .then(response => {
+    
+        const start=Date.now();
+        var now=start;
+        if (response == 'granted') {
+          var ws;
+          ws=new WebSocket(URL);
+          window.addEventListener('devicemotion', (e) => {
+            // do something with e
+            document.getElementById('time').innerText=Date.now()-start;
+            document.getElementById('delta_t').innerText=Date.now()-now;
+            now=Date.now();
+            document.getElementById('value_x').innerText=e.acceleration.x;
+            document.getElementById('value_y').innerText=e.acceleration.y;
+            document.getElementById('value_z').innerText=e.acceleration.z;
+            
+
+            ws.onopen=function(){
+              ws.send(e);
+              setTimeout(send, 100)
+              document.getElementById('state').innerText=send;
+            }
+          })
+        }
+      })
+      .catch(console.error)
+  } else {
+    // non iOS 13+
+    document.getElementById('value_x').innerText='error not ios 13+';
+    document.getElementById('value_y').innerText='';
+    document.getElementById('value_z').innerText='';
+  }
+};
