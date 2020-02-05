2783085e956943dceac787cbc75a1bcbb403c7bb
Ryota Kayanoma <z.ryota.kayanoma@gree.net>
2020-02-05 18:44:12 +0900

    scriptを戻した

--- script.js
+++ script.js
@@ -28,70 +28,6 @@ function getURL(){
   // document.getElementById('check').innerText=URL;
 }
 
-function send_data(id){
-  if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    // iOS 13+
-      DeviceMotionEvent.requestPermission()
-      .then(response => {
-    
-        const start=Date.now();
-        var now=start;
-        var data;
-        var times;
-        var accel;
-        if (response == 'granted') {
-          var ws;
-          document.getElementById('console').innerText=ws.readyState;
-          ws=new WebSocket(URL);
-          document.getElementById('console').innerText=ws.readyState;
-            window.addEventListener('devicemotion', (e) => {
-              // do something with e
-              times={
-                "abs_time":Date.now(),
-                "time":Date.now()-start,
-                "delta_t":Date.now()-now
-              }
-              document.getElementById('abs_time').innerText=times.abs_time;
-              document.getElementById('time').innerText=times.time;
-              document.getElementById('delta_t').innerText=times.delta_t;
-              // document.getElementById('time').i.innerText=Date.now()-start;
-              // document.getElementById('delta_t').innerText=Date.now()-now;
-              now=Date.now();
-              accel={
-                "x":e.acceleration.x,
-                "y" :e.acceleration.y,
-                "z" :e.acceleration.z
-              }
-              // data={
-              //   "times":times,
-              //   "accel":accel
-              // }
-              data=[id, times.abs_time, times.time, times.delta_t, accel.x, accel.y, accel.z];
-              
-              document.getElementById('value_x').innerText=accel.x;
-              document.getElementById('value_y').innerText=accel.y;
-              document.getElementById('value_z').innerText=accel.z;
-              // document.getElementById('value_x').innerText=e.acceleration.x;
-              // document.getElementById('value_y').innerText=e.acceleration.y;
-              // document.getElementById('value_z').innerText=e.acceleration.z;
-              
-              ws.send(data);
-              // setTimeout(send, 100)
-              // document.getElementById('state').innerText=send;
-            });
-          // 受け取ったデータからグラフ作成
-          
-        }
-      })
-      .catch(console.error)
-  } else {
-    // non iOS 13+
-    document.getElementById('value_x').innerText='error not ios 13+';
-    document.getElementById('value_y').innerText='';
-    document.getElementById('value_z').innerText='';
-  }
-};
-
 // function send_data(id){
 //   if (typeof DeviceMotionEvent.requestPermission === 'function') {
 //     // iOS 13+
@@ -105,8 +41,9 @@ function send_data(id){
 //         var accel;
 //         if (response == 'granted') {
 //           var ws;
-//           ws=new WebSocket(URL, ['echo-protocol','soap', 'xmpp']);
-//           ws.onopen=function(){
+//           document.getElementById('console').innerText=ws.readyState;
+//           ws=new WebSocket(URL);
+//           document.getElementById('console').innerText=ws.readyState;
 //             window.addEventListener('devicemotion', (e) => {
 //               // do something with e
 //               times={
@@ -139,10 +76,9 @@ function send_data(id){
 //               // document.getElementById('value_z').innerText=e.acceleration.z;
               
 //               ws.send(data);
-//               setTimeout(send, 100)
-//               document.getElementById('state').innerText=send;
+//               // setTimeout(send, 100)
+//               // document.getElementById('state').innerText=send;
 //             });
-//           }
 //           // 受け取ったデータからグラフ作成
           
 //         }
@@ -155,3 +91,67 @@ function send_data(id){
 //     document.getElementById('value_z').innerText='';
 //   }
 // };
+
+function send_data(id){
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    // iOS 13+
+      DeviceMotionEvent.requestPermission()
+      .then(response => {
+    
+        const start=Date.now();
+        var now=start;
+        var data;
+        var times;
+        var accel;
+        if (response == 'granted') {
+          var ws;
+          ws=new WebSocket(URL, ['echo-protocol','soap', 'xmpp']);
+          ws.onopen=function(){
+            window.addEventListener('devicemotion', (e) => {
+              // do something with e
+              times={
+                "abs_time":Date.now(),
+                "time":Date.now()-start,
+                "delta_t":Date.now()-now
+              }
+              document.getElementById('abs_time').innerText=times.abs_time;
+              document.getElementById('time').innerText=times.time;
+              document.getElementById('delta_t').innerText=times.delta_t;
+              // document.getElementById('time').i.innerText=Date.now()-start;
+              // document.getElementById('delta_t').innerText=Date.now()-now;
+              now=Date.now();
+              accel={
+                "x":e.acceleration.x,
+                "y" :e.acceleration.y,
+                "z" :e.acceleration.z
+              }
+              // data={
+              //   "times":times,
+              //   "accel":accel
+              // }
+              data=[id, times.abs_time, times.time, times.delta_t, accel.x, accel.y, accel.z];
+              
+              document.getElementById('value_x').innerText=accel.x;
+              document.getElementById('value_y').innerText=accel.y;
+              document.getElementById('value_z').innerText=accel.z;
+              // document.getElementById('value_x').innerText=e.acceleration.x;
+              // document.getElementById('value_y').innerText=e.acceleration.y;
+              // document.getElementById('value_z').innerText=e.acceleration.z;
+              
+              ws.send(data);
+              setTimeout(send, 100)
+              document.getElementById('state').innerText=send;
+            });
+          }
+          // 受け取ったデータからグラフ作成
+          
+        }
+      })
+      .catch(console.error)
+  } else {
+    // non iOS 13+
+    document.getElementById('value_x').innerText='error not ios 13+';
+    document.getElementById('value_y').innerText='';
+    document.getElementById('value_z').innerText='';
+  }
+};
